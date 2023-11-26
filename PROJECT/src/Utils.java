478c0941f9bb484a16fe0abef14293bcccc11a83
pchu22 <pmdc2001@gmail.com>
2023-11-26 22:39:30 +0000

    Sprint 3

--- PROJECT/src/Utils.java
+++
@@ -1,6 +0,0 @@
-public class Utils {
-    public static int randomHeartbeat() {
-        return (int) (Math.random() * 251); // Generates a random integer value between 0 and 250 (inclusive)
-    }
-    a
-}
